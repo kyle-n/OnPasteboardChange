c1de4c0f445acc7a189845a9b643adb5947d7b87
kyle-n <kylebnazario@gmail.com>
2020-11-18 13:29:28 -0700

    AppKit pasteboard watcher

--- Sources/OnPasteboardChange/PasteboardChangeStore.swift
+++ Sources/OnPasteboardChange/PasteboardChangeStore.swift
@@ -5,13 +5,44 @@
 //  Created by Kyle Nazario on 11/18/20.
 //
 
-#if !os(macOS)
-import UIKit
 import Combine
 
 final class PasteboardChangeStore: ObservableObject {
     
-    // UIKit hooks
+    private var pasteboardChangedSubscription: AnyCancellable? = nil
+    private let callback: PasteboardCallback
+    
+    init(callback: @escaping PasteboardCallback) {
+        self.callback = callback
+    }
+    
+    func initializeSubscriptions() {
+        pasteboardChangedSubscription = PasteboardChangeStore.pasteboardChanged
+            .sink { _ in self.callback() }
+    }
+}
+
+#if os(macOS)
+
+import AppKit
+extension PasteboardChangeStore {
+    private static let pasteboardChanged = Timer.publish(every: 2, on: .current, in: .common)
+        .autoconnect()
+        .map { _ in
+            let count = NSPasteboard.general.changeCount
+            return count
+        }
+        .merge(with: Just(NSPasteboard.general.changeCount))
+        .removeDuplicates()
+        .dropFirst()
+        .map { _ in Void() }
+        .eraseToAnyPublisher()
+}
+
+#else
+
+import UIKit
+extension PasteboardChangeStore {
     private static let pasteboardChangedInApp = NotificationCenter.default.publisher(for: UIPasteboard.changedNotification)
     private static let applicationActived = NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)
     private static var systemPasteboardChangeCount: Int { UIPasteboard.general.changeCount }
@@ -31,18 +62,5 @@ final class PasteboardChangeStore: ObservableObject {
             .map { _ in Void() }
             .eraseToAnyPublisher()
     }()
-    
-    private var pasteboardChangedSubscription: AnyCancellable? = nil
-    private let callback: PasteboardCallback
-    
-    init(callback: @escaping PasteboardCallback) {
-        self.callback = callback
-    }
-    
-    func initializeSubscriptions() {
-        pasteboardChangedSubscription = PasteboardChangeStore.pasteboardChanged
-            .sink { _ in self.callback() }
-    }
 }
-
 #endif

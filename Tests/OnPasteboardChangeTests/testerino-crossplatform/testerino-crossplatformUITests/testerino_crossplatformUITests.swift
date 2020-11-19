75e51f4c53e5bc0f81494bd366fb9ee2c8677077
kyle-n <kylebnazario@gmail.com>
2020-11-19 15:39:40 -0700

    E2E tests copy outside app with custom pb

--- Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
+++ Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
@@ -77,4 +77,26 @@ class testerino_crossplatformUITests: XCTestCase {
         let counter = app.staticTexts["counter"]
         XCTAssertEqual(counter.label, "Changes: 0")
     }
+    
+    func testCopyOutsideAppWithCustomPasteboard() {
+        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
+        app.launch()
+        
+        app.buttons["Toggle Custom"].tap()
+
+        let contacts = XCUIApplication(bundleIdentifier: "com.apple.MobileAddressBook")
+        contacts.launch()
+        let phoneNumber = contacts.tables.staticTexts["(888) 555-5512"]
+        if (!phoneNumber.exists) {
+            contacts.tables["ContactsListView"].staticTexts["John Appleseed"].tap()
+        }
+        phoneNumber.press(forDuration: 2.0);
+        sleep(1)
+        contacts.staticTexts["Copy"].tap()
+
+        app.activate()
+        sleep(1)
+        let counter = app.staticTexts["counter"]
+        XCTAssertEqual(counter.label, "Changes: 0")
+    }
 }

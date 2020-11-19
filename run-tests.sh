493f303aa8779057197dbe70be6a8da91e51083f
kyle-n <kylebnazario@gmail.com>
2020-11-19 13:48:24 -0700

    Cleanup

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,5 @@
-scheme_name='OnPasteboardChange-Package'
 testerino_project='Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatform.xcodeproj'
 
-#xcodebuild build -sdk iphoneos -scheme $scheme_name
-
-# Build testerino app
-# xcodebuild build -sdk iphonesimulator -scheme 'testerino-crossplatform (iOS)' -project $testerino_project
 killall Simulator
 defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false
 open -a Simulator.app
@@ -14,5 +9,3 @@ osascript -e 'tell application "System Events" to keystroke "k" using {command d
 xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' -scheme 'testerino-crossplatform (iOS)' -project $testerino_project
 killall Simulator
 defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool true
-
-# xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' -scheme $scheme_name

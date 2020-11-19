64c27da640ff5f4327cf8c761003c27db8907dbc
kyle-n <kylebnazario@gmail.com>
2020-11-19 14:36:43 -0700

    Merged

--- Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
+++ Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
@@ -11,21 +11,37 @@ class testerino_crossplatformUITests: XCTestCase {
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+        print("----------------------------------------------------------------")
     }
 
-    func testChangesOnInAppCopy() throws {
+//    func testChangesOnInAppCopy() throws {
+//        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
+//        app.launch()
+//        let textView = app.textViews.firstMatch
+//        textView.tap()
+//
+//        app.keys["O"].tap()
+//        app.keys["n"].tap()
+//        app.keys["e"].tap()
+//
+//        textView.doubleTap()
+//        app.staticTexts["Copy"].tap()
+//
+//        let counter = app.staticTexts["counter"]
+//        XCTAssertEqual(counter.label, "Changes: 1")
+//    }
+    
+    func testCopyOutsideApp() {
         let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
         app.launch()
-        let textView = app.textViews.firstMatch
-        textView.tap()
         
-        app.keys["O"].tap()
-        app.keys["n"].tap()
-        app.keys["e"].tap()
-        
-        textView.doubleTap()
-        app.staticTexts["Copy"].tap()
-        
+        let contacts = XCUIApplication(bundleIdentifier: "com.apple.MobileAddressBook")
+        contacts.launch()
+        contacts.tables["ContactsListView"].staticTexts["John Appleseed"].tap()
+        contacts.tables.staticTexts["(888) 555-5512"].press(forDuration: 2.0);
+        contacts.staticTexts["Copy"].tap()
+              
+        app.activate()
         let counter = app.staticTexts["counter"]
         XCTAssertEqual(counter.label, "Changes: 1")
     }

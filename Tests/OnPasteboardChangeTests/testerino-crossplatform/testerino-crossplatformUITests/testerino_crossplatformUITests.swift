f2425f182ab43872b16fd67cf8ea54e486ec1bd8
kyle-n <kylebnazario@gmail.com>
2020-11-19 15:08:32 -0700

    Updates copy in app test

--- Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
+++ Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
@@ -14,35 +14,37 @@ class testerino_crossplatformUITests: XCTestCase {
         print("----------------------------------------------------------------")
     }
 
-//    func testChangesOnInAppCopy() throws {
-//        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
-//        app.launch()
-//        let textView = app.textViews.firstMatch
-//        textView.tap()
-//
-//        app.keys["O"].tap()
-//        app.keys["n"].tap()
-//        app.keys["e"].tap()
-//
-//        textView.doubleTap()
-//        app.staticTexts["Copy"].tap()
-//
-//        let counter = app.staticTexts["counter"]
-//        XCTAssertEqual(counter.label, "Changes: 1")
-//    }
-    
-    func testCopyOutsideApp() {
+    func testChangesOnInAppCopy() throws {
         let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
         app.launch()
+        let textView = app.textViews.firstMatch
+        textView.tap()
+
+        app.keys["O"].tap()
+        app.keys["n"].tap()
+        app.keys["e"].tap()
+
+        textView.doubleTap()
+        app.staticTexts["Copy"].tap()
         
-        let contacts = XCUIApplication(bundleIdentifier: "com.apple.MobileAddressBook")
-        contacts.launch()
-        contacts.tables["ContactsListView"].staticTexts["John Appleseed"].tap()
-        contacts.tables.staticTexts["(888) 555-5512"].press(forDuration: 2.0);
-        contacts.staticTexts["Copy"].tap()
-              
-        app.activate()
+        sleep(1)
+
         let counter = app.staticTexts["counter"]
         XCTAssertEqual(counter.label, "Changes: 1")
     }
+    
+//    func testCopyOutsideApp() {
+//        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
+//        app.launch()
+//
+//        let contacts = XCUIApplication(bundleIdentifier: "com.apple.MobileAddressBook")
+//        contacts.launch()
+//        contacts.tables["ContactsListView"].staticTexts["John Appleseed"].tap()
+//        contacts.tables.staticTexts["(888) 555-5512"].press(forDuration: 2.0);
+//        contacts.staticTexts["Copy"].tap()
+//
+//        app.activate()
+//        let counter = app.staticTexts["counter"]
+//        XCTAssertEqual(counter.label, "Changes: 1")
+//    }
 }
--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,14 @@
 testerino_project='Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatform.xcodeproj'
 
 killall Simulator
+# iphone_id=$(xcrun simctl list | grep "iPhone 11 (" | head -n 2 | tail -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1)
+# xcrun simctl erase $iphone_id
+
 defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false
 open -a Simulator.app
-sleep 6
+sleep 2
 osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
 osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
-xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' -scheme 'testerino-crossplatform (iOS)' -project $testerino_project
+xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12' -scheme 'testerino-crossplatform (iOS)' -project $testerino_project
 killall Simulator
 defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool true

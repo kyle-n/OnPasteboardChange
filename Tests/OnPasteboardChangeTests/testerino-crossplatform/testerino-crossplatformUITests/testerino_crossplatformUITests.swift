8d42939411a620dad5be838b9b4f16228059e532
kyle-n <kylebnazario@gmail.com>
2020-11-19 11:43:57 -0700

    Cleanup

--- Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
+++ Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformUITests/testerino_crossplatformUITests.swift
@@ -11,19 +11,12 @@ class testerino_crossplatformUITests: XCTestCase {
 
     override func setUpWithError() throws {
         continueAfterFailure = false
-        print("1666666666666666666666666666666666666666666666666666")
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testChangesOnInAppCopy() throws {
-        print("19999999999999999999999999999999999999999999999999999999")
         let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
         app.launch()
         let textView = app.textViews.firstMatch
-//        let textView = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
         textView.tap()
         
         app.keys["O"].tap()
@@ -33,18 +26,7 @@ class testerino_crossplatformUITests: XCTestCase {
         textView.doubleTap()
         app.staticTexts["Copy"].tap()
         
-        print("------------------------------------ qq before assert")
         print(app.staticTexts)
         let counter = app.staticTexts["counter"]
-        print(app.staticTexts["Changes: 1"].exists, "999999999999999999")
     }
-
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
 }

e0f1c475b267830f56a676758d546b816e56ef32
kyle-n <kylebnazario@gmail.com>
2020-11-20 10:47:53 -0700

    Copies outside app

--- Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformMacOSUITests/testerino_crossplatformMacOSUITests.swift
+++ Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatformMacOSUITests/testerino_crossplatformMacOSUITests.swift
@@ -31,15 +31,83 @@ class testerino_crossplatformMacOSUITests: XCTestCase {
         textView.typeKey("a", modifierFlags:.command)
         textView.typeKey("c", modifierFlags:.command)
     }
+    
+    private func copyOutsideApp() {
+        let textEdit = XCUIApplication(bundleIdentifier: "com.apple.TextEdit")
+        textEdit.launch()
+        
+        textEdit.typeKey("n", modifierFlags: .command)
+        let textView = textEdit.textViews.firstMatch
+        textView.typeText("two")
+        textView.typeKey("a", modifierFlags: .command)
+        textView.typeKey("c", modifierFlags: .command)
+        
+        textView.typeKey("q", modifierFlags: .command)
+        textView.typeKey(.delete, modifierFlags: .command)
+    }
 
-    func testExample() throws {
+//    func testChangesOnInAppCopy() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        copyInApp()
+//
+//        sleep(3)
+//        let counter = app.staticTexts["counter"]
+//        XCTAssertEqual(counter.value as! String, "Changes: 1")
+//    }
+    
+    func testCopyOutsideApp() {
         let app = XCUIApplication()
         app.launch()
 
-        copyInApp()
-        
+        copyOutsideApp()
+
+        app.activate()
         sleep(3)
         let counter = app.staticTexts["counter"]
         XCTAssertEqual(counter.value as! String, "Changes: 1")
     }
+    
+    func testCopyInAppToGeneralWithCustomPasteboard() {
+                
+//        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
+//        app.launch()
+//
+//        app.buttons["Toggle Custom"].tap()
+//
+//        copyInApp(app: app)
+//
+//        sleep(1)
+//
+//        let counter = app.staticTexts["counter"]
+//        XCTAssertEqual(counter.label, "Changes: 0")
+    }
+    
+    func testCopyOutsideAppWithCustomPasteboard() {
+//        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
+//        app.launch()
+//
+//        app.buttons["Toggle Custom"].tap()
+//
+//        copyOutsideApp()
+//
+//        app.activate()
+//        sleep(1)
+//        let counter = app.staticTexts["counter"]
+//        XCTAssertEqual(counter.label, "Changes: 0")
+    }
+    
+    func testCopyInAppToCustomPasteboard() {
+//        let app = XCUIApplication(bundleIdentifier: "com.kylenazario.testerino-crossplatform")
+//        app.launch()
+//
+//        app.buttons["Toggle Custom"].tap()
+//        app.buttons["Add to Custom Pasteboard"].tap()
+//
+//        sleep(1)
+//        let counter = app.staticTexts["counter"]
+//        XCTAssertEqual(counter.label, "Changes: 1")
+    }
+
 }

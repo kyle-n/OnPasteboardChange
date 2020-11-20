69feffdd3539eaeb749ed85ea9980f3a5d8bdc5a
kyle-n <kylebnazario@gmail.com>
2020-11-20 10:04:45 -0700

    [WIP] Tests on mac

--- run-tests.sh
+++ run-tests.sh
@@ -2,16 +2,18 @@
 
 testerino_project='Tests/OnPasteboardChangeTests/testerino-crossplatform/testerino-crossplatform.xcodeproj'
 
-killall Simulator
+#killall Simulator
 # iphone_id=$(xcrun simctl list | grep "iPhone 11 (" | head -n 2 | tail -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1)
 # xcrun simctl erase $iphone_id
 
-defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false
-open -a Simulator.app
-sleep 2
-osascript -e 'tell application "Simulator" to activate'
-osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
-osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
-xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' -scheme 'testerino-crossplatform (iOS)' -project $testerino_project
-killall Simulator
-defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool true
+#defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false
+#open -a Simulator.app
+#sleep 2
+#osascript -e 'tell application "Simulator" to activate'
+#osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
+#osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
+#xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' -scheme 'testerino-crossplatform (iOS)' -project $testerino_project
+#killall Simulator
+#defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool true
+
+xcodebuild test -sdk iphonesimulator -destination 'platform=OS X,arch=x86_64' -scheme 'testerino-crossplatform (macOS)' -project $testerino_project

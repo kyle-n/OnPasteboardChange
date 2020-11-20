c7fd978605a534fdcc7fefc56940cda1b0dac92f
kyle-n <kylebnazario@gmail.com>
2020-11-20 11:46:00 -0700

    Stops version update on error

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-./run-tests.sh
-git tag $1
-echo "Set version $1"
+test_failed_lines=$(./run-tests.sh | grep -c "** TEST FAILED **")
+if (( $test_failed_lines > 0 )); then
+  echo ERROR: Aborting version update
+  exit 1
+else
+  git tag $1
+  echo "Set version $1"
+fi
